0c16f3e90fd18857469eaf6dde56aa5978889721
Artur Beljajev <artur.beljajev@internet.ee>
2017-04-27 11:21:38 +0300

    Revert "Merge pull request #469 from internetee/change-registrar-area-controller-hierarchy"
    This reverts commit dc32e96ae544f933c489f013f6e60cc143323ded, reversing
    changes made to 693737fcdda9cca48828cf3cd0fa93f78ca8bfd7.

--- app/controllers/registrar/deposits_controller.rb
+++ app/controllers/registrar/deposits_controller.rb
@@ -1,28 +1,26 @@
-class Registrar
-  class DepositsController < BaseController
-    authorize_resource class: false
+class Registrar::DepositsController < RegistrarController
+  authorize_resource class: false
 
-    def new
-      @deposit = Deposit.new
+  def new
+    @deposit = Deposit.new
+  end
+
+  def create
+    @deposit = Deposit.new(deposit_params.merge(registrar: current_user.registrar))
+    @invoice = @deposit.issue_prepayment_invoice
+
+    if @invoice && @invoice.persisted?
+      flash[:notice] = t(:please_pay_the_following_invoice)
+      redirect_to [:registrar, @invoice]
+    else
+      flash.now[:alert] = t(:failed_to_create_record)
+      render 'new'
     end
+  end
 
-    def create
-      @deposit = Deposit.new(deposit_params.merge(registrar: current_user.registrar))
-      @invoice = @deposit.issue_prepayment_invoice
+  private
 
-      if @invoice && @invoice.persisted?
-        flash[:notice] = t(:please_pay_the_following_invoice)
-        redirect_to [:registrar, @invoice]
-      else
-        flash.now[:alert] = t(:failed_to_create_record)
-        render 'new'
-      end
-    end
-
-    private
-
-    def deposit_params
-      params.require(:deposit).permit(:amount, :description)
-    end
+  def deposit_params
+    params.require(:deposit).permit(:amount, :description)
   end
 end
